88943a8d57d23b323c38db20fbdcd4ac7ed71fae
dey4ss <27929897+dey4ss@users.noreply.github.com>
2025-01-13 17:08:27 +0100

    another round of mem limit

--- scripts/greenplum_configure.sh
+++ scripts/greenplum_configure.sh
@@ -20,20 +20,23 @@ gp_home="$(pwd)/db_comparison_data/greenplum"
 # gp_vmem approx. 300 GB
 # gp_vmem_protect_limit = gp_vmem / max_acting_primary_segments  --> 27 segments
 # gp_vmem_protect_limit = 11.11... GiB --> 11378 MiB
-PGPORT=${PORT} GPHOME="${gp_home}" COORDINATOR_DATA_DIRECTORY="${gp_home}/data/gpseg-1"  "${gp_home}/bin/gpconfig" -c gp_vmem_protect_limit -v 21943
+PGPORT=${PORT} GPHOME="${gp_home}" COORDINATOR_DATA_DIRECTORY="${gp_home}/data/gpseg-1"  "${gp_home}/bin/gpconfig" -c gp_vmem_protect_limit -v 50000
+sleep 10
 
 # Reload config.
 GPHOME="${gp_home}" "${gp_home}/bin/gpstop" -d "${gp_home}/data/gpseg-1" -u
+sleep 10
 
 "${gp_home}/bin/psql" -p "${PORT}" -d dbbench -f "$(pwd)/scripts/gp_reload.sql"
 
 # Restart to apply all changes (esp. memory limit).
+echo "Restart to set memory limit ..."
+sleep 10
 GPHOME="${gp_home}" "${gp_home}/bin/gpstop" -a -d "${gp_home}/data/gpseg-1" -r
 
 
 # We observed that changing the memory limit on all segments can require multiple restarts to be effective.
-while ! PGPORT=${PORT} GPHOME="${gp_home}" COORDINATOR_DATA_DIRECTORY="${gp_home}/data/gpseg-1"  "${gp_home}/bin/gpconfig" -s gp_vmem_protect_limit | grep "Values on all segments are consistent"; do
+while sleep 10 && ! PGPORT=${PORT} GPHOME="${gp_home}" COORDINATOR_DATA_DIRECTORY="${gp_home}/data/gpseg-1"  "${gp_home}/bin/gpconfig" -s gp_vmem_protect_limit | grep "Values on all segments are consistent"; do
   echo "Restart to set memory limit ..."
-  sleep 5
   GPHOME="${gp_home}" "${gp_home}/bin/gpstop" -a -d "${gp_home}/data/gpseg-1" -r
 done
